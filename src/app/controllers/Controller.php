4078d69b04f147dc05c64b26b93b2ce6f92d75ed
felipesantos2 <santospinheiro627@gmail.com>
2025-09-21 13:45:34 -0300

    wip

---
+++ src/app/controllers/Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\controllers;
+
+class Controller
+{
+    public function __contruct() {}
+
+    public function index(): void {}
+    public function create(): void {}
+    public function store(): void {}
+    public function show(): void {}
+    public function edit(): void {}
+    public function destroy(): void {}
+}
---
+++ src/app/controllers/HomeController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\controllers;
+
+use app\controllers\Controller;
+
+class HomeController extends Controller
+{
+    
+}
